ed8ce8afe4f3db34746b60bd78ff0725b6154f42
lubien <lubien1996@gmail.com>
2026-03-21 17:17:16 -0300

    Format precommit alias and add warnings-as-errors to test

--- pretex/mix.exs
+++ pretex/mix.exs
@@ -93,7 +93,12 @@ defmodule Pretex.MixProject do
         "esbuild pretex --minify",
         "phx.digest"
       ],
-      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"]
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "test --warnings-as-errors"
+      ]
     ]
   end
 end
