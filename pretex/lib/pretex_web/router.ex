1f12cf265db4af35d58f6f75be42915171f1dc63
Iago Cavalcante <iagocavalcante@hey.com>
2026-03-23 15:12:36 -0300

    feat: add check-in LiveView with QR scanner, search, and nav link

--- pretex/lib/pretex_web/router.ex
+++ pretex/lib/pretex_web/router.ex
@@ -166,6 +166,8 @@ defmodule PretexWeb.Router do
       live("/organizations/:org_id/events/:event_id/orders/new", OrderLive.NewManual, :new_manual)
       live("/organizations/:org_id/events/:event_id/orders/:id", OrderLive.Show, :show)
 
+      live("/organizations/:org_id/events/:event_id/check-in", CheckInLive.Index, :index)
+
       live("/organizations/:org_id/events/:event_id/fees", FeeLive.Index, :index)
       live("/organizations/:org_id/events/:event_id/fees/new", FeeLive.Index, :new)
       live("/organizations/:org_id/events/:event_id/fees/:id/edit", FeeLive.Index, :edit)
